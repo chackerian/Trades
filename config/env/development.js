a8955036bd0c30e9221dda5e890b956df04761e1
chackerian <nchackerian@gmail.com>
2014-11-08 23:31:52 -0500

    Hooked up DB with mongo hq heroku add-on

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://localhost/mean-dev1',
+  db: 'mongodb://heroku:3c7fa42afb6f3a6ffa87f574c4480658@dogen.mongohq.com:10035/app31419162',
 	debug: 'true',
   mongoose: {
     debug: false
