0a0b245c507849154e4f360b2bf62afe5c3db8eb
chackerian <nchackerian@gmail.com>
2014-11-20 07:38:26 -0500

    Edited colors directive which crrently isn't working

--- packages/system/public/controllers/index.js
+++ packages/system/public/controllers/index.js
@@ -1,4 +1,3 @@
-'use strict';
 
 angular.module('mean.system').
   controller('IndexController', ['$scope', 'Global',
@@ -35,3 +34,28 @@ angular.module('mean.system').
       // });
     }
 ]);
+angular.module('mean.system').
+      directive('color', ['$scope', function($scope) {
+                        // Runs during compile
+                        return {
+                              // name: '',
+                              // priority: 1,
+                              // terminal: true,
+                              // scope: {}, // {} = isolate, true = child, false/undefined = no change
+                              // controller: function($scope, $element, $attrs, $transclude) {},
+                              // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+                              restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+                              template: '<div class="square">HAHFHALJ</div>',
+                              // templateUrl: '',
+                              //replace: true,
+                              // transclude: true,
+                              // compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+                              //link: function($scope, iElm, iAttrs, controller) {
+                                    /* jshint ignore:start */
+                                   // $scope.randomColor = Math.floor(Math.random()*16777215).toString(16);
+                                    /* jshint ignore:end */
+                              //angular.element('<span class="glyphicon glyphicon-usd rand"></span>')[0].css('color', '#' + randomColor;);
+
+                        //}
+                   };
+                  }]);         
--- packages/system/public/directives/color.js
+++ packages/system/public/directives/color.js
@@ -1,6 +1,6 @@
 'use strict';
-angular.module('mean.system').
-	directive('color', ['$scope', function($scope){
+angular.module('mean.system')
+.directive('color', ['$scope', function($scope) {
 				// Runs during compile
 				return {
 					// name: '',
@@ -9,18 +9,18 @@ angular.module('mean.system').
 					// scope: {}, // {} = isolate, true = child, false/undefined = no change
 					// controller: function($scope, $element, $attrs, $transclude) {},
 					// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-					restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
-					//template: '<',
+					restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+					template: '<div class="square">HAHFHALJ</div>',
 					// templateUrl: '',
 					//replace: true,
 					// transclude: true,
 					// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
 					link: function($scope, iElm, iAttrs, controller) {
 						/* jshint ignore:start */
-						var randomColor = Math.floor(Math.random()*16777215).toString(16);
+						$scope.randomColor = Math.floor(Math.random()*16777215).toString(16);
 						/* jshint ignore:end */
      			 		//angular.element('<span class="glyphicon glyphicon-usd rand"></span>')[0].css('color', '#' + randomColor;);
-						
-					}
-				};
-			}]);		
+
+     			 	}
+     			 };
+     			}]);		
