6a64e2e57d02101ec468fa7d4e410821b096c936
chackerian <nchackerian@gmail.com>
2014-11-20 23:34:24 -0500

    Revert "Removed random string" Reverting This reverts commit 9ba4d2440d9e1306e5aa63d12492c440626b8f4e.

--- config/env/all.js
+++ config/env/all.js
@@ -79,6 +79,7 @@ module.exports = {
   db: process.env.MONGOHQ_URL,
   //Enables swig templating engine 
   templateEngine: 'swig',
+  buhBaby:"bubuhbuhbuhbaby",
   // The secret should be set to a non-guessable string
   // Used to compute a session hash
   sessionSecret: 'MEAN',
