38e762bdf4363c8f34a61e995976538576a37088
chackerian <nchackerian@gmail.com>
2014-11-21 22:45:31 -0500

    changed directive and controller

--- packages/system/public/controllers/index.js
+++ packages/system/public/controllers/index.js
@@ -7,33 +7,6 @@ angular.module('mean.system').
       $scope.shoes = ['/theme/assets/img/nike-1.jpg','/theme/assets/img/nike-2.jpg'];
       $scope.randomcolor = Math.floor(Math.random()*16777215).toString(16);
       $scope.a = document.getElementsByClassName('snoop');
-      // $scope.place = angular.element()[0];
 
-      // $( '.sidebar' ).simpleSidebar({
-      //       settings: {
-      //             opener: '#open-sb',
-      //             wrapper: '.wrapper',
-      //             animation: {
-      //                   duration: 500,
-      //                   easing: 'easeOutQuint'
-      //             }
-      //       },
-      //       sidebar: {
-      //             align: 'left',
-      //             width: 200,
-      //             closingLinks: 'a',
-      //       }
-      // });
-      // $scope.$watch(function () {
-      //   for (var i = 0; i < $scope.sites.length; i+=1) {
-      //     if ($scope.sites[i].active) {
-      //       return $scope.sites[i];
-      //     }
-      //   }
-      // }, function (currentSlide, previousSlide) {
-      //   if (currentSlide !== previousSlide) {
-      //     console.log('currentSlide:', currentSlide);
-      //   }
-      // });
     }
 ]);        
