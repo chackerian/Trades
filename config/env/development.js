4486c4b146c19aec165717e0e4564c62b70d1e68
chackerian <nchackerian@gmail.com>
2014-11-22 00:49:28 -0500

    Changed config files

--- config/env/development.js
+++ config/env/development.js
@@ -15,22 +15,22 @@ module.exports = {
     callbackURL: 'http://localhost:3000/auth/facebook/callback'
   },
   twitter: {
-    clientID: 'DEFAULT_CONSUMER_KEY',
+    clientID: 'CONSUMER_KEY',
     clientSecret: 'CONSUMER_SECRET',
     callbackURL: 'http://localhost:3000/auth/twitter/callback'
   },
   github: {
-    clientID: 'DEFAULT_APP_ID',
+    clientID: 'APP_ID',
     clientSecret: 'APP_SECRET',
     callbackURL: 'http://localhost:3000/auth/github/callback'
   },
   google: {
-    clientID: 'DEFAULT_APP_ID',
+    clientID: 'APP_ID',
     clientSecret: 'APP_SECRET',
     callbackURL: 'http://localhost:3000/auth/google/callback'
   },
   linkedin: {
-    clientID: 'DEFAULT_API_KEY',
+    clientID: 'API_KEY',
     clientSecret: 'SECRET_KEY',
     callbackURL: 'http://localhost:3000/auth/linkedin/callback'
   },
