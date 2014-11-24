2b5eb12b0cc4b6f336a6f1ba8964f47636c179b5
chackerian <nchackerian@gmail.com>
2014-11-24 04:36:20 -0500

    Added issues creator and css

---
+++ issue.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#curl --user "caspyin" --request POST --data '{"description":"Created via API","public":"true","files":{"file1.txt":{"content":"Demo"}}' https://api.github.com/gists
+echo\
+ {\
+  "title": $(date | cut -d ":" -f1)\
+} > json.txt
+curl --user "chackerian" --request POST --data @json.txt https://api.github.com/repos/chackerian/Trades/issues
