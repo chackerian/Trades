c04a755aeb562ec3691bbc1931e1a8cf07eca61f
chackerian <nchackerian@gmail.com>
2014-12-14 17:37:25 -0500

    Added new listing files

--- packages/custom/listing/public/controllers/listing.js
+++ packages/custom/listing/public/controllers/listing.js
@@ -1,10 +1,83 @@
 'use strict';
 
-angular.module('mean.listing').controller('ListingController', ['$scope', 'Global', 'Listing',
-  function($scope, Global, Listing) {
+angular.module('mean.listing').controller('ListingController', ['$scope', '$stateParams', '$location', 'Global', 'Listing',
+  function($scope, $stateParams, $location, Global, Listing) {
     $scope.global = Global;
     $scope.package = {
       name: 'listing'
     };
+
+    $scope.hasAuthorization = function(article) {
+      if (!article || !article.user) return false;
+      return $scope.global.isAdmin || article.user._id === $scope.global.user._id;
+    };
+
+    $scope.create = function(isValid) {
+      if (isValid) {
+        var article = new Articles({
+          title: this.title,
+          content: this.content,
+          price: this.price
+
+        });
+        article.$save(function(response) {
+          $location.path('articles/' + response._id);
+        });
+
+        this.title = '';
+        this.content = '';
+        this.price = '';
+
+      } else {
+        $scope.submitted = true;
+      }
+    };
+
+    $scope.remove = function(article) {
+      if (article) {
+        article.$remove();
+
+        for (var i in $scope.articles) {
+          if ($scope.articles[i] === article) {
+            $scope.articles.splice(i, 1);
+          }
+        }
+      } else {
+        $scope.article.$remove(function(response) {
+          $location.path('articles');
+        });
+      }
+    };
+
+    $scope.update = function(isValid) {
+      if (isValid) {
+        var article = $scope.article;
+        if (!article.updated) {
+          article.updated = [];
+        }
+        article.updated.push(new Date().getTime());
+
+        article.$update(function() {
+          $location.path('articles/' + article._id);
+        });
+      } else {
+        $scope.submitted = true;
+      }
+    };
+
+    $scope.find = function() {
+      Articles.query(function(articles) {
+        $scope.articles = articles;
+      });
+    };
+
+    $scope.findOne = function() {
+      Articles.get({
+        articleId: $stateParams.articleId
+      }, function(article) {
+        $scope.article = article;
+      });
+    };
   }
 ]);
+
