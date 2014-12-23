4298904cd23e39e673c5d5adafd6a3391abf529a
chackerian <nchackerian@gmail.com>
2014-12-22 19:08:51 -0500

    PIONEER

--- packages/access/server/config/passport.js
+++ packages/access/server/config/passport.js
@@ -2,11 +2,11 @@
 
 var mongoose = require('mongoose'),
   LocalStrategy = require('passport-local').Strategy,
-  TwitterStrategy = require('passport-twitter').Strategy,
+  // TwitterStrategy = require('passport-twitter').Strategy,
   FacebookStrategy = require('passport-facebook').Strategy,
-  GitHubStrategy = require('passport-github').Strategy,
-  GoogleStrategy = require('passport-google-oauth').OAuth2Strategy,
-  LinkedinStrategy = require('passport-linkedin').Strategy,
+  // GitHubStrategy = require('passport-github').Strategy,
+  // GoogleStrategy = require('passport-google-oauth').OAuth2Strategy,
+  // LinkedinStrategy = require('passport-linkedin').Strategy,
   User = mongoose.model('User'),
   config = require('meanio').loadConfig();
 
@@ -55,39 +55,39 @@ module.exports = function(passport) {
   ));
 
   // Use twitter strategy
-  passport.use(new TwitterStrategy({
-      consumerKey: config.twitter.clientID,
-      consumerSecret: config.twitter.clientSecret,
-      callbackURL: config.twitter.callbackURL
-    },
-    function(token, tokenSecret, profile, done) {
-      User.findOne({
-        'twitter.id_str': profile.id
-      }, function(err, user) {
-        if (err) {
-          return done(err);
-        }
-        if (user) {
-          return done(err, user);
-        }
-        user = new User({
-          name: profile.displayName,
-          username: profile.username,
-          provider: 'twitter',
-          twitter: profile._json,
-          roles: ['authenticated']
-        });
-        user.save(function(err) {
-          if (err) {
-            console.log(err);
-            return done(null, false, {message: 'Twitter login failed, email already used by other login strategy'});
-          } else {
-            return done(err, user);
-          }
-        });
-      });
-    }
-  ));
+  // passport.use(new TwitterStrategy({
+  //     consumerKey: config.twitter.clientID,
+  //     consumerSecret: config.twitter.clientSecret,
+  //     callbackURL: config.twitter.callbackURL
+  //   },
+  //   function(token, tokenSecret, profile, done) {
+  //     User.findOne({
+  //       'twitter.id_str': profile.id
+  //     }, function(err, user) {
+  //       if (err) {
+  //         return done(err);
+  //       }
+  //       if (user) {
+  //         return done(err, user);
+  //       }
+  //       user = new User({
+  //         name: profile.displayName,
+  //         username: profile.username,
+  //         provider: 'twitter',
+  //         twitter: profile._json,
+  //         roles: ['authenticated']
+  //       });
+  //       user.save(function(err) {
+  //         if (err) {
+  //           console.log(err);
+  //           return done(null, false, {message: 'Twitter login failed, email already used by other login strategy'});
+  //         } else {
+  //           return done(err, user);
+  //         }
+  //       });
+  //     });
+  //   }
+  // ));
 
   // Use facebook strategy
   passport.use(new FacebookStrategy({
@@ -111,7 +111,8 @@ module.exports = function(passport) {
           username: profile.username || profile.emails[0].value.split('@')[0],
           provider: 'facebook',
           facebook: profile._json,
-          roles: ['authenticated']
+          roles: ['authenticated'],
+          photo: profile.photos
         });
         user.save(function(err) {
           if (err) {
@@ -126,101 +127,101 @@ module.exports = function(passport) {
   ));
 
   // Use github strategy
-  passport.use(new GitHubStrategy({
-      clientID: config.github.clientID,
-      clientSecret: config.github.clientSecret,
-      callbackURL: config.github.callbackURL
-    },
-    function(accessToken, refreshToken, profile, done) {
-      User.findOne({
-        'github.id': profile.id
-      }, function(err, user) {
-        if (user) {
-          return done(err, user);
-        }
-        user = new User({
-          name: profile.displayName,
-          email: profile.emails[0].value,
-          username: profile.username,
-          provider: 'github',
-          github: profile._json,
-          roles: ['authenticated']
-        });
-        user.save(function(err) {
-          if (err) {
-            console.log(err);
-            return done(null, false, {message: 'Github login failed, email already used by other login strategy'});
-          } else {
-            return done(err, user);
-          }
-        });
-      });
-    }
-  ));
+  // passport.use(new GitHubStrategy({
+  //     clientID: config.github.clientID,
+  //     clientSecret: config.github.clientSecret,
+  //     callbackURL: config.github.callbackURL
+  //   },
+  //   function(accessToken, refreshToken, profile, done) {
+  //     User.findOne({
+  //       'github.id': profile.id
+  //     }, function(err, user) {
+  //       if (user) {
+  //         return done(err, user);
+  //       }
+  //       user = new User({
+  //         name: profile.displayName,
+  //         email: profile.emails[0].value,
+  //         username: profile.username,
+  //         provider: 'github',
+  //         github: profile._json,
+  //         roles: ['authenticated']
+  //       });
+  //       user.save(function(err) {
+  //         if (err) {
+  //           console.log(err);
+  //           return done(null, false, {message: 'Github login failed, email already used by other login strategy'});
+  //         } else {
+  //           return done(err, user);
+  //         }
+  //       });
+  //     });
+  //   }
+  // ));
 
   // Use google strategy
-  passport.use(new GoogleStrategy({
-      clientID: config.google.clientID,
-      clientSecret: config.google.clientSecret,
-      callbackURL: config.google.callbackURL
-    },
-    function(accessToken, refreshToken, profile, done) {
-      User.findOne({
-        'google.id': profile.id
-      }, function(err, user) {
-        if (user) {
-          return done(err, user);
-        }
-        user = new User({
-          name: profile.displayName,
-          email: profile.emails[0].value,
-          username: profile.emails[0].value,
-          provider: 'google',
-          google: profile._json,
-          roles: ['authenticated']
-        });
-        user.save(function(err) {
-          if (err) {
-            console.log(err);
-            return done(null, false, {message: 'Google login failed, email already used by other login strategy'});
-          } else {
-            return done(err, user);
-          }
-        });
-      });
-    }
-  ));
+  // passport.use(new GoogleStrategy({
+  //     clientID: config.google.clientID,
+  //     clientSecret: config.google.clientSecret,
+  //     callbackURL: config.google.callbackURL
+  //   },
+  //   function(accessToken, refreshToken, profile, done) {
+  //     User.findOne({
+  //       'google.id': profile.id
+  //     }, function(err, user) {
+  //       if (user) {
+  //         return done(err, user);
+  //       }
+  //       user = new User({
+  //         name: profile.displayName,
+  //         email: profile.emails[0].value,
+  //         username: profile.emails[0].value,
+  //         provider: 'google',
+  //         google: profile._json,
+  //         roles: ['authenticated']
+  //       });
+  //       user.save(function(err) {
+  //         if (err) {
+  //           console.log(err);
+  //           return done(null, false, {message: 'Google login failed, email already used by other login strategy'});
+  //         } else {
+  //           return done(err, user);
+  //         }
+  //       });
+  //     });
+  //   }
+  // ));
 
   // use linkedin strategy
-  passport.use(new LinkedinStrategy({
-      consumerKey: config.linkedin.clientID,
-      consumerSecret: config.linkedin.clientSecret,
-      callbackURL: config.linkedin.callbackURL,
-      profileFields: ['id', 'first-name', 'last-name', 'email-address']
-    },
-    function(accessToken, refreshToken, profile, done) {
-      User.findOne({
-        'linkedin.id': profile.id
-      }, function(err, user) {
-        if (user) {
-          return done(err, user);
-        }
-        user = new User({
-          name: profile.displayName,
-          email: profile.emails[0].value,
-          username: profile.emails[0].value,
-          provider: 'linkedin',
-          roles: ['authenticated']
-        });
-        user.save(function(err) {
-          if (err) {
-            console.log(err);
-            return done(null, false, {message: 'LinkedIn login failed, email already used by other login strategy'});
-          } else {
-            return done(err, user);
-          }
-        });
-      });
-    }
-  ));
+  // passport.use(new LinkedinStrategy({
+  //     consumerKey: config.linkedin.clientID,
+  //     consumerSecret: config.linkedin.clientSecret,
+  //     callbackURL: config.linkedin.callbackURL,
+  //     profileFields: ['id', 'first-name', 'last-name', 'email-address']
+  //   },
+  //   function(accessToken, refreshToken, profile, done) {
+  //     User.findOne({
+  //       'linkedin.id': profile.id
+  //     }, function(err, user) {
+  //       if (user) {
+  //         return done(err, user);
+  //       }
+  //       user = new User({
+  //         name: profile.displayName,
+  //         email: profile.emails[0].value,
+  //         username: profile.emails[0].value,
+  //         provider: 'linkedin',
+  //         roles: ['authenticated']
+  //       });
+  //       user.save(function(err) {
+  //         if (err) {
+  //           console.log(err);
+  //           return done(null, false, {message: 'LinkedIn login failed, email already used by other login strategy'});
+  //         } else {
+  //           return done(err, user);
+  //         }
+  //       });
+  //     });
+  //   }
+  // ));
 };
--- packages/custom/listing/public/controllers/listing.js
+++ packages/custom/listing/public/controllers/listing.js
@@ -1,8 +1,21 @@
 'use strict';
 
 angular.module('mean.listing').controller('ListingController', ['$scope', 'Global', 'Listing',
-  function($scope, Global, Listing) {
+  function ($scope, Global, Listing) {
     $scope.global = Global;
+
+    // $scope.hasAuthorization = function(listing) {
+    // 	if (!listing || !listing.user) return false;
+    // 	return $scope.global.isAdmin || listing.user._id === $scope.global.user._id;
+    // }
+    // $scope.create = function(isValid){
+    // 	if (isValid) {
+    // 		var listing = new Listings({
+    			
+    // 		})
+    // 	}
+    // }
+
     $scope.package = {
       name: 'listing'
     };
